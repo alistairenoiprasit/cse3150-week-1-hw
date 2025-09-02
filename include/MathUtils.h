1f9056a7adef440b9db8eb0da3782c01837c8174
Alistaire Noiprasit <alistaire.noiprasit@gmail.com>
2025-09-02 10:22:43 -0400

    Add simple math program homework

---
+++ include/MathUtils.h
@@ -0,0 +1,13 @@
+//
+// Created by Alistaire Noiprasit on 29/8/2025.
+//
+
+#ifndef MATHUTILS_H
+#define MATHUTILS_H
+
+#endif //MATHUTILS_H
+
+namespace MathUtils {
+    int add(int a, int b);
+    int multiply(int a, int b);
+}
---
+++ src/MathUtils.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Alistaire Noiprasit on 29/8/2025.
+//
+#include "../include/MathUtils.h"
+
+using namespace MathUtils;
+
+int MathUtils::add(int a, int b) {
+    return a + b;
+}
+
+int MathUtils::multiply(int a, int b) {
+    return a * b;
+}
